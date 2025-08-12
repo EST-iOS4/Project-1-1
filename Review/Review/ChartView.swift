ecda66aabd870ec8d79ee2cbd6a9c5648fce8455
Song Young Min <iasdsr1347@naver.com>
2025-08-12 15:20:26 +0900

    ChartView.swift 뷰 선언, 화면 나눔 및 다크모드 대응

--- Review/Review/ChartView.swift
+++ Review/Review/ChartView.swift
@@ -7,118 +7,36 @@
 
 import SwiftUI
 
-struct ChartView: View {
-  @Environment(\.horizontalSizeClass) var horizontalSizeClass
-
+class ChartViewModel: ObservableObject {
   @AppStorage("isDarkMode") var isDarkMode = false
-
+  
   let markedDates: Set<Date>
-
-  @State private var currentMonth: Date = Date()
+  
+  @Published private var currentMonth: Date = Date()
   private let calendar: Calendar = .current
-
-  var body: some View {
-    let isPad = horizontalSizeClass == .regular
-    VStack {
-      VStack {
-        Divider()
-        Text("활동 내역")
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .padding(10)
-          .font(.system(size: isPad ? 40 : 20))
-
-        HStack(spacing: isPad ? 60 : 20) {
-          Button(action: { changeMonth(by: -1) }) {
-            Image(systemName: "chevron.left")
-              .bold()
-              .font(.system(size: isPad ? 50 : 20))
-          }
-          //        Spacer()
-          Text(monthTitle(for: currentMonth))
-            .font(.system(size: isPad ? 50 : 20))
-            .bold()
-          //        Spacer()
-          Button(action: { changeMonth(by: 1) }) {
-            Image(systemName: "chevron.right")
-              .bold()
-              .font(.system(size: isPad ? 50 : 20))
-          }
-        }
-
-        // 요일 날짜 넣기
-        let daysWeek = ["일", "월", "화", "수", "목", "금", "토"]
-
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
-          ForEach(daysWeek, id: \.self) { day in
-            Text(day)
-              .font(.system(size: isPad ? 40 : 14))
-              .foregroundColor(
-                day == "일" ? .red : (day == "토" ? .blue : .primary)
-              )
-          }
-        }
-        //    .padding(.horizontal)
-
-        // 달력 날짜 생성
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
-          // ForEach(arrayMonthDays(for: currentMonth), id: \.self) { date in
-          ForEach(0..<arrayMonthDays(for: currentMonth).count, id: \.self) {
-            index in
-            if let date = arrayMonthDays(for: currentMonth)[index] {
-              //if let date = date {
-              let cellDate = calendar.startOfDay(for: date)
-              DayCell(
-                date: date,
-                isMarked: markedDates.contains(cellDate),
-                size: isPad ? 80 : 40
-              )
-              //            .onTapGesture {
-              //                   showFullJandiblock = true
-              //         Text("\(calendar.component(.day, from: date))")
-              //           .frame(width: 40, height: 40)
-
-            } else {
-              Color.clear.frame(height: isPad ? 60 : 40)
-            }
-          }
-        }
-        Divider()
-
-        Text("키워드 통계")
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .padding(10)
-          .font(.system(size: isPad ? 40 : 20))
-      }
-      .padding(.horizontal)
-      .preferredColorScheme(isDarkMode ? .dark : .light)
-    }
-    .frame(maxHeight: .infinity, alignment: .top)
+  
+  init(markedDates: Set<Date>) {
+    self.markedDates = markedDates
   }
-
-  // 달 바꾸기
-  func changeMonth(by value: Int) {
-    if let newMonth = calendar.date(
-      byAdding: .month,
-      value: value,
-      to: currentMonth
-    ) {
-      currentMonth = newMonth
-    }
-  }
-
+  
   // 달 년도 표시
-  func monthTitle(for date: Date) -> String {
+  var monthTitle: String {
     let formatter = DateFormatter()
     formatter.locale = Locale(identifier: "ko_KR")
     formatter.dateFormat = "yyyy년   M월"
-    return formatter.string(from: date)
+    return formatter.string(from: currentMonth)
   }
-
-  // 달력 배열 만들기
-  func arrayMonthDays(for month: Date) -> [Date?] {
-    guard let range = calendar.range(of: .day, in: .month, for: month),
+  
+  // 요일 표시
+  var dayWeeks: [String] {
+    ["일", "월", "화", "수", "목", "금", "토"]
+  }
+  
+  //
+  var arrayMonthDays: [Date?] {
+    guard let range = calendar.range(of: .day, in: .month, for: currentMonth),
       let firstDay = calendar.date(
-        from: calendar.dateComponents([.year, .month], from: month)
+        from: calendar.dateComponents([.year, .month], from: currentMonth)
       )
     else { return [] }
 
@@ -138,6 +56,110 @@ struct ChartView: View {
     }
 
     return days
+  }
+  
+  // 달 바꾸기
+  func changeMonth(by value: Int) {
+    if let newMonth = calendar.date(byAdding: .month, value: value, to: currentMonth) {
+      currentMonth = newMonth
+    }
+  }
+  
+  // 작성한 날짜
+  func isDateMarked(_ date: Date?) -> Bool {
+    guard let date else { return false }
+    return markedDates.contains(calendar.startOfDay(for: date))
+  }
+  
+}
+
+
+struct ChartView: View {
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  @StateObject private var viewModel: ChartViewModel
+  
+  init(markedDates: Set<Date>) {
+    _viewModel = StateObject(wrappedValue: ChartViewModel(markedDates: markedDates))
+  }
+
+  var body: some View {
+    let isPad = horizontalSizeClass == .regular
+    
+    ScrollView {
+      VStack {
+        Divider()
+          .background(viewModel.isDarkMode ? Color.white.opacity(1) : Color.gray)
+        
+        Text("활동 내역")
+       .frame(maxWidth: .infinity, alignment: .leading)
+          .font(.system(size: isPad ? 40 : 20))
+          .padding(10)
+         
+        HStack(spacing: isPad ? 60 : 20) {
+          Button { viewModel.changeMonth(by: -1)
+          } label: {
+            Image(systemName: "chevron.left")
+              .bold()
+              .font(.system(size: isPad ? 50 : 20))
+          }
+          
+          
+          Text(viewModel.monthTitle)
+            .font(.system(size: isPad ? 50 : 20))
+            .bold()
+          
+          
+          Button{ viewModel.changeMonth(by: 1) } label: {
+            Image(systemName: "chevron.right")
+              .bold()
+              .font(.system(size: isPad ? 50 : 20))
+          }
+        }
+
+        // 요일 날짜 넣기
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
+          ForEach(viewModel.dayWeeks, id: \.self) { day in
+            Text(day)
+              .font(.system(size: isPad ? 40 : 14))
+              .foregroundColor(
+                day == "일" ? .red : (day == "토" ? .blue : .primary)
+              )
+          }
+        }
+        //    .padding(.horizontal)
+
+        // 달력 날짜 생성
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
+          ForEach(Array(viewModel.arrayMonthDays.enumerated()), id: \.offset) { index, date in
+            if let date {
+              DayCell(
+                date: date,
+                isMarked: viewModel.isDateMarked(date),
+                size: isPad ? 80 : 40
+              )
+              //            .onTapGesture {
+              //                   showFullJandiblock = true
+              //         Text("\(calendar.component(.day, from: date))")
+              //           .frame(width: 40, height: 40)
+            } else {
+              Color.clear.frame(height: isPad ? 60 : 40)
+            }
+          }
+        }
+        .padding(.bottom, 20)
+        Divider()
+          .background(viewModel.isDarkMode ? Color.white.opacity(1) : Color.gray)
+
+        Text("키워드 통계")
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .font(.system(size: isPad ? 40 : 20))
+          .padding(10)
+          
+      }
+      .padding(.horizontal)
+      
+    }
+    .preferredColorScheme(viewModel.isDarkMode ? .dark : .light)
   }
 }
 
