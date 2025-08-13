26f06e11c2ebef85fb8da5bc9661dde37521c9cf
남병수 <nambyoungsu@gmail.com>
2025-08-13 11:22:38 +0900

    Model파일로 공용 모델 이동,  ListView에서 공용 모델 삭제, 메모 삭제 기능 추가 및 UI 개선

--- Review/Review/Model.swift
+++ Review/Review/Model.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct Memo: Identifiable {
+    let id: UUID
+    var day: Date
+    var tags: [String]
+    var content: String
+
+    init(id: UUID = UUID(), day: Date, tags: [String], content: String) {
+        self.id = id
+        self.day = day
+        self.tags = tags
+        self.content = content
+    }
+}
+
+func formatDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter.string(from: date)
+}
+
+enum Screen {
+    case memoList
+    case statistics
+    case settings
+}
